02ddc92e904169da64b64feef5b0e77c8223d546
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-10-25 00:30:53 -0500

    Update search.php

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
 	<link rel="icon" href="images/headlogo.png">
 </head>
 <body>
-<center><h1 style="font-size: 40px;">There are no results found for this search.<br><a style="text-decoration: none; position: relative; margin-left: -75px;" href="/index"><img style="width: 30px; height: auto;  margin-top: -5px; transform: rotate(180deg);"src="images/back.png"><b id="back" style="color: blue; position: absolute; margin-top: -1.5px; margin-left: 5px;"> Back</b></a></h1><br><br><br><br><br><br></center>
+<center><h1 style="font-size: 40px;">There are no results found for this search.<br><a style="text-decoration: none; position: relative; margin-left: -75px;" href="/index.php"><img style="width: 30px; height: auto;  margin-top: -5px; transform: rotate(180deg);"src="images/back.png"><b id="back" style="color: blue; position: absolute; margin-top: -1.5px; margin-left: 5px;"> Back</b></a></h1><br><br><br><br><br><br></center>
 <?php
 
 ?>
