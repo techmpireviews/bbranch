2fb4500119cb986d67e926df1eb341b90398c079
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-10-25 10:41:15 -0500

    Update search.php

--- search.php
+++ search.php
@@ -7,7 +7,11 @@
 <body>
 <center><h1 style="font-size: 40px;">There are no results found for this search.<br><a style="text-decoration: none; position: relative; margin-left: -75px;" href="/index.php"><img style="width: 30px; height: auto;  margin-top: -5px; transform: rotate(180deg);"src="images/back.png"><b id="back" style="color: blue; position: absolute; margin-top: -1.5px; margin-left: 5px;"> Back</b></a></h1><br><br><br><br><br><br></center>
 <?php
+$result= $_POST['search'];
+$result= strtolower($result);
 
+if (strpos($result, "home") == "true")
+header ('Location: /?search=home.php;
 ?>
 </body>
 </html>
