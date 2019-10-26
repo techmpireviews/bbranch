3e026396878eafa1f237f207fae90184ed9c4c77
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-10-25 22:57:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,29 +27,6 @@
 				<img id="searchimg" src="images/searchicon.png"></span>
 		</div>
 <style type="text/css">
-#infotop {
-	position: absolute;
-	top: 15px;
-	right: 4px;
-        clip-path: circle(21px at 272px 22px);
-	background-color: #ffffff;
-	transition: 0.5s;
-	padding: 5px 8px;
-	z-index: 15;
-}
-
-#infotop:hover {
-	position: absolute;
-	top: 15px;
-	right: 4px;
-	border-radius: 30px;
-	display: inline-block;
-	background-color: #ffffff;
-        clip-path: circle(100%);
-        transition: 0.5s;
-	padding: 5px 8px;
-	z-index: 15;
-}
 						#searchid {
 							height: 17px;
 							width: 168px;
